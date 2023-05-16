f749fa78c2d5fbbfa37673229c4e55d3d30c8816
Gibran Hakim <90363814+gevabriel@users.noreply.github.com>
2023-05-16 14:39:17 +0700

    update read.c

--- 9_Input_Output/read.c
+++ 9_Input_Output/read.c
@@ -2,13 +2,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+
 int main()
 {
-    char c;
+    char c[42];
+
     int fd1 = open("sample.txt", O_RDONLY, 0);
-    int fd2 = open("sample.txt", O_RDONLY, 0);
-    read(fd1, &c, 1);
-    read(fd2, &c, 1);
-    printf("c = %c\n", c);
+    // int fd2 = open("sample2.txt", O_RDONLY, 0);
+
+    read(fd1, &c, 42);
+    // read(fd2, &c, 3);
+
+    for (int i = 0; i < sizeof(c); i++)
+    {
+        printf("%c ", c[i]);
+    }
+    printf("\n");
+
     exit(0);
 }
